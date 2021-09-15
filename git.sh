90131ee97a3402f76c0d6c04b5d732e4daabeb16
fd7c6e1f <email@example.org>
2021-09-15 20:51:42 +0400

    $comment

---
+++ git.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo 'Название коммита'
+git add .
+read comment
+git commit -m '$comment'
+
+
+while true; do
+    read -p "Пушим? [Y/n]" yn
+    case $yn in
+        [Yy]* ) git push; break;;
+        [Nn]* ) exit;;
+        * ) git push; break;;
+    esac
+done
